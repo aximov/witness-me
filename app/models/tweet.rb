37c2da2f71d0ae98dbf4799dc1c597efe05df59d
Akimoto Daichi <d@alumni.tus.ac.jp>
2020-04-19 21:31:07 +0900

    validation の書き方を修正、追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
-    validates :url, presence: true, format: { with: /twitter.com/, message: "Twitter の URL である必要があります"}
+    validates :url, presence: true
+    validates :url, format: { with: /twitter.com/, message: "Twitter の URL である必要があります。"}
+    validates :url, format: { with: /https/, message: "https から入力してください。"}
 end
