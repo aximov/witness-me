a6ff4698a2a6279ee16e89faa1447658116fc055
Akimoto Daichi <d@alumni.tus.ac.jp>
2020-04-19 22:39:18 +0900

    Tweet モデルのテストを追加

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save tweet without URL" do
+    tweet = Tweet.new
+    assert_not tweet.save, "Saved the tweet without an URL"
+  end
 end
